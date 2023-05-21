89708a1528aa908f2cb2888ce19fca3bb8c10a51
Simon Brundin <simonbrundin@gmail.com>
2023-05-21 13:00:46 +0200

    back

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import { fileURLToPath } from "url";
-import { dirname, join } from "path";
-const currentDir = dirname(fileURLToPath(import.meta.url));
 
 export default defineNuxtConfig({
   modules: ["@pinia/nuxt", "@nuxtjs/apollo", "@unocss/nuxt", "@nuxt/devtools"],
 
   css: [
     // SCSS file in the project
-    join(currentDir, "./assets/main.css"),
+    "@/assets/css/main.scss",
   ],
   unocss: {
     // presets
