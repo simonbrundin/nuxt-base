2620727908fac2d18599c5cbb6917e20e2011e99
Simon Brundin <simonbrundin@gmail.com>
2023-10-23 12:05:05 +0200

    updates

--- auth/nuxt.config.ts
+++ auth/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   extends: ["gh:simonbrundin/nuxt-layers/base"],
-  modules: [],
+  modules: ["@pinia/nuxt"],
   imports: {
     dirs: ["./stores"],
   },
--- test/nuxt.config.ts
+++ test/nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  extends: ["../auth", "../base"],
+  extends: [
+    [
+      "gh:simonbrundin/nuxt-layers/auth",
+      { giget: { auth: process.env.GITHUB_AUTH } },
+    ],
+  ],
   modules: ["@nuxt/image"],
 });
