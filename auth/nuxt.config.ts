8e4a363d5ac401cd1039223af3e57fd42a1f7722
Simon Brundin <simonbrundin@gmail.com>
2023-10-23 12:15:46 +0200

    giget

--- auth/nuxt.config.ts
+++ auth/nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  extends: ["gh:simonbrundin/nuxt-layers/base"],
+  extends: [
+    [
+      "gh:simonbrundin/nuxt-layers/base",
+      { giget: { auth: process.env.GITHUB_AUTH } },
+    ],
+  ],
   imports: {
     dirs: ["./stores"],
   },
