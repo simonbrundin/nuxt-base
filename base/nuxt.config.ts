9d9c1b223da2babeaa3c1c25db829498ee4b9474
Simon Brundin <simonbrundin@gmail.com>
2023-10-23 12:10:34 +0200

    tested removing pinia

--- base/nuxt.config.ts
+++ base/nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@pinia/nuxt", "@unocss/nuxt", "@nuxt/image"],
+  modules: ["@unocss/nuxt", "@nuxt/image"],
   devtools: { enabled: true },
   plugins: ["~/plugins/debounce"],
 });
