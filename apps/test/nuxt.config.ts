1e54a71ac6e37a8af0e00a47a62ef2e00c5966bf
Simon Brundin <simonbrundin@gmail.com>
2023-10-23 18:22:29 +0200

    updated extends to gh

--- apps/test/nuxt.config.ts
+++ apps/test/nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  extends: ["../../auth"],
+  extends: ["gh:simonbrundin/nuxt-layers/auth"],
   modules: ["@nuxt/image"],
 });
