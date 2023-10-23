1806f13b2b51b6058c7f015280eb62c24e995b8b
Simon Brundin <simonbrundin@gmail.com>
2023-10-23 12:11:24 +0200

    removing pinia

--- auth/nuxt.config.ts
+++ auth/nuxt.config.ts
@@ -1,7 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   extends: ["gh:simonbrundin/nuxt-layers/base"],
-  modules: ["@pinia/nuxt"],
   imports: {
     dirs: ["./stores"],
   },
