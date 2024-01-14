8dbe0f9f5d0dd051c7d31a68bfd7747823b9723a
Simon Brundin <simonbrundin@gmail.com>
2024-01-14 16:32:04 +0100

    added Tairo

--- base/nuxt.config.ts
+++ base/nuxt.config.ts
@@ -3,4 +3,10 @@ export default defineNuxtConfig({
   modules: ["@unocss/nuxt", "@nuxt/image"],
   devtools: { enabled: true },
   plugins: ["~/plugins/debounce"],
+  extends: [
+    "../tairo/layers/tairo",
+    "../tairo/layers/tairo-layout-collapse",
+    "../tairo/layers/tairo-layout-sidebar",
+    "../tairo/layers/tairo-layout-topnav",
+  ],
 });
